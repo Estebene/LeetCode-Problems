b4a0d21447acd91187696f82deaded3a2e794624
Steven Little <49330942+Estebene@users.noreply.github.com>
2022-09-30 22:52:39 +1300

    Time: 4086 ms (26.54%), Space: 14.2 MB (85.52%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,11 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        previous_nums = dict()
+        """
+         previous_nums = dict()
         for i, num in enumerate(nums):
             if (target - num) in previous_nums:
                 return (previous_nums[target-num], i)
             previous_nums[num] = i
+        """
+        for idx, num in enumerate(nums):
+            for idx2, num2 in enumerate(nums):
+                if num + num2 == target and idx != idx2:
+                    return idx, idx2
         return None
             
         
