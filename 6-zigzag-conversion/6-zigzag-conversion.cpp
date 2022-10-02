83ca303fe27dfd75891e61c9a95a29247507bf32
Steven Little <49330942+Estebene@users.noreply.github.com>
2022-10-02 21:49:56 +1300

    Time: 246 ms (5.00%), Space: 10.6 MB (54.45%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int mod = (numRows - 1) * 2;
+        if (mod == 0) {
+            mod = 1;
+        }
+        int index = 0;
+        vector<string> ss(numRows);
+        for (char c: s) {
+            if (index % mod == 0) {
+                ss[0].push_back(c);
+            } else {
+                int num = index % mod;
+                if (num > mod / 2) {
+                    num = mod - num;
+                }
+                cout << index % mod;
+                cout << " ";
+                cout << num;
+                cout << "\n";
+                ss[num].push_back(c);
+            }
+            index ++;
+        }
+        string ns = "";
+        for (int n = 0; n < numRows; n++) {
+            ns += ss[n];
+        }
+        return ns;
+    }
+};
