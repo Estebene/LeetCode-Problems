802daa749a40db5a4e805ce9b2ea197a57ca8a0f
Steven Little <49330942+Estebene@users.noreply.github.com>
2022-10-10 06:19:19 +1300

    Time: 94 ms (31.56%), Space: 14.5 MB (41.42%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,16 +1,12 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        
-        @lru_cache(None)
-        def dp(i, canBuy):
-            if i == n:
-                return 0
-            ans = dp(i+1, canBuy)  # Skip
-            if canBuy:
-                ans = max(ans, dp(i+1, False) - prices[i])  # Buy
-            else:
-                ans = max(ans, dp(i+1, True) + prices[i])  # Sell
-            return ans
-        
-        return dp(0, True)
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        total, last = 0, prices[0]
+        for price in prices[1:]:
+            if price > last:
+                total += (price-last)
+            last = price
+        return total
